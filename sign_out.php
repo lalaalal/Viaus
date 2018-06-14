1512724e5a2f691fecff853a1cb0b99db09d9b45
lalaalal <39375299+lalaalal@users.noreply.github.com>
2018-06-14 20:25:52 +0900

    exit

--- sign_out.php
+++ sign_out.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
+session_unset();
 $res=session_destroy();
 if ($res) {
   sleep(1);
   header ('Location: index.html');
 } else {
   echo "session_destroy failed";
+  exit();
 }
 ?>

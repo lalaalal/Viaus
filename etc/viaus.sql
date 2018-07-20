69841f0ede7c596d2a95f59edbef9a31f234a960
lalaalal <100madalala@gmail.com>
2018-07-20 18:19:25 +0900

    .

--- etc/viaus.sql
+++ etc/viaus.sql
@@ -3,11 +3,12 @@ CREATE DATABASE viaus;
 USE viaus;
 
 CREATE TABLE user_data (
-  `no` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `id` CHAR(16) NOT NULL PRIMARY KEY,
+  `no` INT NOT NULL AUTO_INCREMENT,
+  `id` CHAR(16) NOT NULL,
   `pw` CHAR(16) NOT NULL,
   `name` CHAR(16) NULL,
   `sex` CHAR(5) NULL,
   `date` DATETIME NOT NULL DEFAULT NOW(),
-  `class` CHAR(10) NOT NULL DEFAULT 'user'
+  `class` CHAR(10) NOT NULL DEFAULT 'user',
+  PRIMARY KEY (no, id)
 );

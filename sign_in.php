1d13e976f31c7c95d28b7f14ad0bebf1c69df3e2
lalaalal <100madalala@gmail.com>
2018-08-05 18:00:35 +0900

    Recognize capital letters on MySQL

--- sign_in.php
+++ sign_in.php
@@ -4,7 +4,7 @@ include './lib/check.php';
 $mysqli = mysqli_connect("localhost", "viaus", "isdj_viaus", "viaus");
 $F_POST = form_check($mysqli, $_POST);
 
-$query = "SELECT * FROM user_data WHERE id='{$F_POST['id']}' AND pw='{$F_POST['pw']}'";
+$query = "SELECT * FROM user_data WHERE BINARY id='{$F_POST['id']}' AND pw='{$F_POST['pw']}'";
 $result = $mysqli->query($query);
 
 if ($result->num_rows == 1) {

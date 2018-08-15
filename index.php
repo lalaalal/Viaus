f6702c76ac6f0db4bdcae37291ba2c78135b0d9c
lalaalal <100madalala@gmail.com>
2018-08-15 12:48:02 +0900

    .

--- index.php
+++ index.php
@@ -17,5 +17,5 @@ if (!isset($_SESSION['id'])) {
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="./js/script.js"></script>
   </head>
-<?php require("./template/$proc.php"); ?>
+<?php require("./body/$proc.php"); ?>
 </html>
